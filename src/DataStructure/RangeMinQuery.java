657a029921453ff3746bd70d6ded33a46613b947
nehaChopra <nchopra456@gmail.com>
2019-11-20 16:23:32 +0530

    Day 1

--- src/DataStructure/RangeMinQuery.java
+++ src/DataStructure/RangeMinQuery.java
@@ -1,4 +1,4 @@
-//https://www.geeksforgeeks.org/segment-tree-set-1-sum-of-given-range/
+//https://www.geeksforgeeks.org/segment-tree-set-1-range-minimum-query/
 package DataStructure;
 
 import java.util.Arrays;
