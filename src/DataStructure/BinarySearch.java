b5bc1828163c0180b3ea12d8722a54ebf47f775f
nehaChopra <nchopra456@gmail.com>
2020-02-09 22:19:30 +0530

    100 days of coding

--- src/DataStructure/BinarySearch.java
+++ src/DataStructure/BinarySearch.java
@@ -3,10 +3,23 @@
  */
 package DataStructure;
 
+import java.util.Scanner;
+
 public class BinarySearch {
 	public static void main(String[] args) {
-		int arr[] = { 2, 3, 4, 10, 40 };
-		System.out.println(binarySearch(arr, 0, (arr.length-1), 10));
+//		int arr[] = { 2, 3, 4, 10, 40 };
+//		System.out.println(binarySearch(arr, 0, (arr.length-1), 10));
+        Scanner input = new Scanner(System.in);
+		int testCases = input.nextInt();
+		for(int test = 0; test<testCases; test++){
+			int arrayLength = input.nextInt();
+			int array[] = new int[arrayLength];
+			int element = input.nextInt();
+			for(int index=0; index<arrayLength; index++){
+				array[index] = input.nextInt();
+			}
+			System.out.println(binarySearch(array,0,(array.length-1),element));
+		}
 	}
 
 	public static int binarySearch(int[] array, int start, int end, int ele) {
--- src/DataStructure/ElementSortedRotatedArray.java
+++ src/DataStructure/ElementSortedRotatedArray.java
@@ -3,14 +3,27 @@
  */
 package DataStructure;
 
+import java.util.Scanner;
+
 public class ElementSortedRotatedArray {
 	public static void main(String[] args) {
 		int arr[] = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
-		System.out.println(binarySearch(arr, 0, arr.length, 6));
+		System.out.println(binarySearch(arr, 0, arr.length-1, 9));
+//        Scanner input = new Scanner(System.in);
+//		int testCases = input.nextInt();
+//		for(int test = 0; test<testCases; test++){
+//			int arrayLength = input.nextInt();
+//			int array[] = new int[arrayLength];
+//			for(int index=0; index<arrayLength; index++){
+//				array[index] = input.nextInt();
+//			}
+//			int element = input.nextInt();
+//			System.out.println(binarySearch(array,0,(array.length-1),element));
+//		}
 	}
 
 	public static int binarySearch(int[] array, int start, int end, int ele) {
-		if (start < end) {
+		if (start <= end) {
 			int midIndex = start + (end - start) / 2;
 			if (array[midIndex] == ele) {
 				return midIndex;
